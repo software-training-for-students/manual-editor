359f6806e642758e03df3aa0bc4f64c5b912758e
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-03-03 14:05:01 -0600

    Added styles and common key bindings to the RTE.

--- components/RichTextEditor.tsx
+++ components/RichTextEditor.tsx
@@ -11,13 +11,27 @@ interface Props {
 
 class RichTextEditor extends React.Component<Props, void> {
     public render() {
-        return <DraftJs.Editor editorState={this.props.value}
-                    onChange={this.onChange}
-                    placeholder={"Type your content below."} />
+        return (
+            <div className="editor">
+                <DraftJs.Editor editorState={this.props.value}
+                        onChange={this.onChange}
+                        placeholder={"Type your content below."}
+                        handleKeyCommand={this.handleKeyCommand} />
+            </div>
+        );
     }
 
     private onChange = (value : DraftJs.EditorState) => {
         this.props.onValueChange(value);
+    }
+
+    private handleKeyCommand : (command : string) => "handled" | "not-handled" = (command : string) => {
+        var state = DraftJs.RichUtils.handleKeyCommand(this.props.value, command);
+        if(state) {
+            this.onChange(state);
+            return "handled";
+        }
+        return "not-handled";
     }
 }
 
