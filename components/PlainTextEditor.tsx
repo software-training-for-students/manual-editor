380de6f4e9442e98a3e4382c0e8635c6b776add8
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-03-04 16:25:05 -0600

    Added the ability to insert tabs into the Raw HTML and Code editors.

--- components/PlainTextEditor.tsx
+++ components/PlainTextEditor.tsx
@@ -10,11 +10,22 @@ interface Props {
 
 class PlainTextEditor extends React.Component<Props, void> {
 public render() {
-        return <textarea onChange={this.onChange} value={this.props.value} {... this.props} />;
+        return <textarea onChange={this.onChange} value={this.props.value} {... this.props} onKeyDownCapture={this.onKeyDown} />;
     }
 
     private onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         this.props.onValueChange(event.target.value);
+    }
+
+    private onKeyDown = (event: React.SyntheticEvent<HTMLTextAreaElement>) => {
+        let nativeEvent = event.nativeEvent as KeyboardEvent;
+        if (nativeEvent.keyCode === 9) { // Tab Key
+            event.preventDefault();
+            let selectionStart = event.currentTarget.selectionStart;
+            let selectionEnd = event.currentTarget.selectionEnd;
+            this.props.onValueChange(this.props.value.substring(0, selectionStart) + "\t" + this.props.value.substring(selectionEnd));
+            event.currentTarget.selectionStart = event.currentTarget.selectionEnd = selectionStart + 1;
+        }
     }
 }
 
