97114e1bb594e48b152e64c2a32cd64559f77d43
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-03-14 10:23:26 -0500

    Refactored code to cleanup webpack config.

--- components/SingleLineEditor.tsx
+++ components/SingleLineEditor.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import AutoUnfocusEditor from "./enhancers/AutoUnfocusEditor";
-import DeletableEditor from "./enhancers/DeletableEditor";
 
 type EditorProps = React.HTMLAttributes<HTMLInputElement> & {
     value: string;
