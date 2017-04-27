4c6204158ef7f6dc7ed77c3243bf299d8448c62f
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-04-26 21:58:35 -0500

    Refactored out the container elements into a factory in createElement.tsx.

--- core/createElement.tsx
+++ core/createElement.tsx
@@ -1,9 +1,4 @@
 import removable from "components/higher-order/removable";
-import * as Lists from "components/Lists";
-import Table from "components/Table";
-import TableCell from "components/TableCell";
-import TableHeader from "components/TableHeader";
-import TableRow from "components/TableRow";
 import EditableCode from "containers/editors/EditableCode";
 import EditableHeading from "containers/editors/EditableHeading";
 import EditableHtml from "containers/editors/EditableHtml";
@@ -16,13 +11,21 @@ import EditableToolbox from "containers/editors/EditableToolbox";
 import * as React from "react";
 import { ItemTree } from "./../stores/Document";
 
-type EditableProps = {
+type EditableElementProps = {
     itemId: number;
     items?: ItemTree[];
 };
 
+function MetaElement(Container: React.ComponentClass<void> | React.SFC<void>) {
+    return (props: EditableElementProps) => (
+        <Container>
+            {props.items && props.items.map(createElement)}
+        </Container>
+    );
+}
+
 interface ElementTypes {
-    [name: string]: React.ComponentClass<EditableProps> | React.StatelessComponent<EditableProps>;
+    [name: string]: React.ComponentClass<EditableElementProps> | React.StatelessComponent<EditableElementProps>;
 }
 
 let elementTypes: ElementTypes = {
@@ -34,20 +37,18 @@ let elementTypes: ElementTypes = {
     ["Code"] : removable(EditableCode),
     ["SidebarNote"] : removable(EditableSidebarNote),
     ["Toolbox"]: removable(EditableToolbox),
-    ["UnorderedList"] : Lists.UnorderedList,
-    ["OrderedList"] : Lists.OrderedList,
-    ["InstructionList"] : Lists.InstructionList,
+    ["UnorderedList"] : MetaElement(({children}) => <ul>{children}</ul>),
+    ["OrderedList"] : MetaElement(({children}) => <ol>{children}</ol>),
+    ["InstructionList"] : MetaElement(({children}) => <ol className="instruction-list">{children}</ol>),
     ["ListItem"] : removable(EditableListItem),
-    ["Table"] : removable(Table),
-    ["TableRow"] : TableRow,
-    ["TableCell"] : TableCell,
-    ["TableHeader"] : TableHeader,
+    ["Table"] : removable(MetaElement(({children}) => <table><tbody>{children}</tbody></table>)),
+    ["TableRow"] : MetaElement(({children}) => <tr>{children}</tr>),
+    ["TableCell"] : MetaElement(({children}) => <td>{children}</td>),
+    ["TableHeader"] : MetaElement(({children}) => <th>{children}</th>),
     ["KeyboardShortcut"]: removable(EditableKeyboardShortcut),
 };
 
-function createElement(props: ItemTree) {
+export default function createElement(props: ItemTree) {
     const Element = elementTypes[props.elementType];
     return <Element itemId={props.itemId} key={props.itemId} items={props.items} />;
 }
-
-export default createElement;
